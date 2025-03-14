b6b0645b720ec7e460cb03a521bc5ef72e5a2a0c
takahashi-N <naotaka022@gmail.com>
2025-03-14 13:28:48 +0900

    Get URLs by tab operation

--- scripts/background.js
+++ scripts/background.js
@@ -1,12 +1,31 @@
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.query({}, (tabs) => {
-      console.log('List of URLs for currently open tabs:');
+function logAllTabsWithCurrentFlag() {
+  chrome.tabs.query({}, (tabs) => {
+    console.log('Tab state update:');
+    
+    chrome.tabs.query({ active: true, currentWindow: true }, (activeTabs) => {
+      const currentTabId = activeTabs[0]?.id;
+
       tabs.forEach((tab, index) => {
         if (tab.url) {
-          console.log(`${index + 1}. ${tab.url}`);
+          const isCurrent = tab.id === currentTabId ? '[CURRENT]' : '';
+          console.log(`${index + 1}. ${tab.url} ${isCurrent}`);
         }
       });
     });
+  });
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'complete') {
+    logAllTabsWithCurrentFlag();
   }
 });
+
+// タブを選択した時
+chrome.tabs.onActivated.addListener(logAllTabsWithCurrentFlag);
+// 別windowへ移動時
+chrome.tabs.onAttached.addListener(logAllTabsWithCurrentFlag);
+// windowから切り離し時
+chrome.tabs.onDetached.addListener(logAllTabsWithCurrentFlag);
+// 同一window内で位置変更時
+chrome.tabs.onMoved.addListener(logAllTabsWithCurrentFlag);
